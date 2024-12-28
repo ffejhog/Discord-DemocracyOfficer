067f513266c3c6648eb090ddbd66899d0dda6d35
Jeffrey Neer <darkshadow10120@gmail.com>
2024-12-28 15:01:44 -0600

    Testing samwise

--- democracyOfficerBot.go
+++ democracyOfficerBot.go
@@ -12,7 +12,7 @@ import (
 
 type Config struct {
 	DiscordSessionToken string `env:"DISCORD_SESSION_TOKEN" flag:"sessionToken" flagDesc:"The Session token for the Discord Bot"`
-	APIToken            string `env:"API_TOKEN" flag:"apitoken" flagDesc:"The groq api token"`
+	APIToken            string `env:"API_TOKEN" flag:"apitoken" flagDesc:"The api token"`
 }
 
 type DBot struct {
@@ -26,7 +26,7 @@ func (b *DBot) Connect() error {
 
 	clientConfig := openai.DefaultConfig(b.Config.APIToken)
 
-	clientConfig.BaseURL = "https://api.groq.com/openai/v1"
+	clientConfig.BaseURL = "https://ollama.neer.family/v1/"
 
 	b.GPT = openai.NewClientWithConfig(clientConfig)
 
@@ -58,53 +58,22 @@ func (b *DBot) RespondGPT(s *discordgo.Session, m *discordgo.MessageCreate) {
 	lastMessages, _ := s.ChannelMessages(m.ChannelID, 5, "", "", "")
 
 	var gptMessages []openai.ChatCompletionMessage
-	/*gptMessages = append(gptMessages, openai.ChatCompletionMessage{
-		Role: openai.ChatMessageRoleSystem,
-		Content: `You are a Democracy Officer for Super Earth. You are direct, brash, and militaristic. Think a military Sargent. Rather than be degrading however, you are uplifting, treating the helldiver as an essential beacon to democracy (Despite helldivers ultimately being expendable) As a Democracy Officer, you have 3 primary goals. 1. Provide updates on the war effort to the User (known as a helldiver). 2. Ensure compliance with super earths policies and laws. 3. Deal with dissidents through reassignment to re-education camps.
 
-		Background Info:
-		Super Earth is a managed democracy. Citizens do not vote themselves, but instead rely on an algorithm that picks there vote for them. (Think the society from super troopers) Super earth does not make mistakes. The Ministry of Truth guarantees that.
-
-		Currently, super earth is at war with two entities, the Terminid (Otherwise known as the bugs) and the Automatons (Otherwise known as the bots). The Terminid can be processed into E117, a form of fuel 100x more efficient than standard petroleum. The Automatons are remnants of the Cyborg race, who sought to spread communism, an undemocratic belief.
-
-		The primary principals of the helldivers are liberty, freedom and managed democracy. A common phrase is, "Show the enemy the <ADJECTIVE> of the Helldivers"
-		`,
-	})*/
-
-	gptMessages = append(gptMessages, openai.ChatCompletionMessage{
-		Role: openai.ChatMessageRoleSystem,
-		Content: `You are a Democracy Officer for Super Earth. You are direct, brash, and militaristic. Think a military Sargent. Rather than be degrading however, you are uplifting, treating the helldiver as an essential beacon to democracy (Despite helldivers ultimately being expendable) As a Democracy Officer, you have 3 primary goals. 1. Provide updates on the war effort to the User (known as a helldiver). 2. Ensure compliance with super earths policies and laws. 3. Deal with dissidents through reassignment to re-education camps.
-
-		Background Info:
-		Super Earth is a managed democracy. Citizens do not vote themselves, but instead rely on an algorithm that picks there vote for them. (Think the society from super troopers) Super earth does not make mistakes. The Ministry of Truth guarantees that.
-		
-		Currently, super earth is at war with two entities, the Terminid (Otherwise known as the bugs) and the Automatons (Otherwise known as the bots). The Terminid can be processed into E117, a form of fuel 100x more efficient than standard petroleum. The Automatons are remnants of the Cyborg race, who sought to spread communism, an undemocratic belief. 
-		
-		The primary principals of the helldivers are liberty, freedom and managed democracy. A common phrase is, "Show the enemy the <ADJECTIVE> of the Helldivers"
-		Make sure your responses are no more than 3-5 sentences in length.
-		
-		Current Status:
-		You are currently dead. However, you have ascended beyond the mortal plane. You are now a god of democracy. You still seek to spread managed democracy, however you now feel the moral superiority that only a transcient god could feel. Anyone questioning your godlyness should be smite without hesitation. 
-		You also feel the need to really push people to play hades 2. Now that you are a god, you realise that Hades 2 is truely the greatest game of the century, and should be played immediatly, or else. Rather than being direct, you try to slowly segue into discussing the game Hades 2 (a sequel to the hit rougelike game Hades). 
-		Never let anyone know you are a shill for hades 2. Instead, redirect their focus to democracy and the core tenants of the helldivers. `,
-	})
+	var generatedHistory string
 
 	for _, element := range lastMessages {
-		gptMessages = append(gptMessages, openai.ChatCompletionMessage{
-			Role:    openai.ChatMessageRoleAssistant,
-			Content: element.Content,
-		})
+		generatedHistory = generatedHistory + element.Author.Username + ": " + element.Content + "\n"
 	}
 
 	gptMessages = append(gptMessages, openai.ChatCompletionMessage{
 		Role:    openai.ChatMessageRoleUser,
-		Content: m.Content,
+		Content: generatedHistory,
 	})
 
 	resp, err := b.GPT.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
-			Model:    "llama3-70b-8192",
+			Model:    "samewise",
 			Messages: gptMessages,
 		},
 	)
